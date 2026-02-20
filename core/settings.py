b76e1a4404efd6b45c70e3ace48677e497668448
beper223 <zashalovskiy@gmail.com>
2026-02-20 14:44:30 +0100

    some fixes

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'news.middleware.LoginRequiredMiddleware'
+    #'news.middleware.LoginRequiredMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
